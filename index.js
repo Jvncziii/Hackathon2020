61ac575cdf72e5973f272261a84200edafe9fb2a
jvncziii <dan.porzucek@gmail.com>
2020-11-28 14:17:48 +0100

    x

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.post('/veriUsr',(req,res)=>{
             {
                 throw err
             }
-            console.log(rows[0].RowDataPacket);
+            console.log(rows[0].NrTel);
             console.log('Jest taki telefon');
         });
     });
